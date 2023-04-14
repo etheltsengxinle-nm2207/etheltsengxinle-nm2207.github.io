bcc2d81837ab9c9bcdf05fc6011535b333fffe28
etheltsengxinle-nm2207 <125366273+etheltsengxinle-nm2207@users.noreply.github.com>
2023-04-14 20:10:21 +0800

    week 13 submission

--- js/radarChart.js
+++ js/radarChart.js
@@ -32,18 +32,18 @@ let radarChart = new Chart("radarChart", {
 });
 
 //add a slider in html with the id "slider"
+var slider = document.getElementById("slider");
 
-//update the graph according to slider value
-function changeSlider () {
-  //get the slider value (a number from 0 to 1)
-  var slidervalue = document.getElementById("slider").value;
-  //console.log(slidervalue);
-  
+//update the graph dynamically according to slider value
+slider.oninput = function() {
+  //console.log(this.value);
+
   //scale the slider value to an integer between 0 and 100
-  var scaledvalue = Math.floor(slidervalue*100);
+  var scaledvalue = Math.floor(this.value*100);
   //console.log(scaledvalue);
 
   //assign it to the data value of the array
+  data.datasets[0].data[0] = coldwar[scaledvalue];
   data.datasets[0].data[0] = coldwar[scaledvalue];
   data.datasets[0].data[1] = utopia[scaledvalue];
   data.datasets[0].data[2] = eden[scaledvalue];
@@ -65,53 +65,9 @@ function changeSlider () {
   //update the chart
   radarChart.update();
 
-  //indicate what year the data is from
+  //indicate what year it is
   let year = scaledvalue + 1900;
   document.getElementById("year").innerHTML = "The year is: " + year;
-  };
-  
 
-//event listener that calls the changeSlider function when slider is changed
-slider.addEventListener ("change", function() {changeSlider()});
-  
+}
 
-
-function next () {
-  //get the slider value (a number from 0 to 1)
-  var slidervalue = document.getElementById("slider").value;
-  //console.log(slidervalue);
-  
-  //scale the slider value to an integer between 0 and 100
-  var scaledvalue = Math.floor(slidervalue*100);
-
-  var newvalue = scaledvalue + 1;
-  console.log (newvalue);
-
-  data.datasets[0].data[0] = coldwar[newvalue];
-  data.datasets[0].data[1] = utopia[newvalue];
-  data.datasets[0].data[2] = eden[newvalue];
-  data.datasets[0].data[3] = amen[newvalue];
-  data.datasets[0].data[4] = sandwich[newvalue];
-  data.datasets[0].data[5] = whiskey[newvalue];
-  data.datasets[0].data[6] = disco[newvalue];
-  data.datasets[0].data[7] = jazz[newvalue];
-
-  data.datasets[1].data[0] = lengzhan[newvalue];
-  data.datasets[1].data[1] = wutuobang[newvalue];
-  data.datasets[1].data[2] = yidianyuan[newvalue];
-  data.datasets[1].data[3] = amen2[newvalue];
-  data.datasets[1].data[4] = sanmingzhi[newvalue];
-  data.datasets[1].data[5] = weishijijiu[newvalue];
-  data.datasets[1].data[6] = disike[newvalue];
-  data.datasets[1].data[7] = jueshiyue[newvalue];
-  
-
-  radarChart.update();
-
-};
-
-
-//To-do: 
-//label steps for slider
-//previous/ next buttons
-//update chart while slider is moving, not when slider stops moving
